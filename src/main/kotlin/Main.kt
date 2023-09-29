30295218acb4abe209937df8a7c763db0ea104eb
William_Sandjon <kevinsandjon01@gmail.com>
2023-09-29 20:20:31 +0200

    essaie

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+  println("hello world!!!")
+    hello()
+
+    // decouvrir pourquoi (presque tout a une valeur)
+
+    //
+
+    println("Program arguments: ${args.joinToString()}")
+}
+fun hello(){
+    println("hello")
+}
+
+g
